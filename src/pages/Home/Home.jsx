eae969f3f8496d5b73178f9508cfb6c3da741dd4
anshugarg401 <92512766+anshugarg401@users.noreply.github.com>
2023-12-06 00:46:53 +0530

    hi

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,146 @@
+import React from 'react';
+import msit from "../../public/msit.png"
+import pic1 from "../../public/pic1.png"
+import pic from "../../public/pic.png"
+import pic2 from "../../public/pic2.png"
+const Home = () => {
+    return <div>
+        <div className="flex flex-row ml-10 lg:ml-12 items-center justify-center space-x-16 md:space-x-24 space-y-1 lg:space-x-36">
+          <div><img src={msit} alt=" "/></div>
+          <div><img src={pic1} alt="" className='   h-30 w-52'/></div>
+          <div><img src={pic} alt="" /></div>
+           
+           
+           
+        </div>
+        <div  className="flex flex-col items-center justify-center  mt-10">
+        <span className='text-2xl sm:text-3xl text-blue-600 lg:text-4xl'>ICAIA 2024</span>
+        <span className=' text-lg lg:text-3xl text-blue-600 px-10'>March 19-20, 2024, New Delhi, India</span>
+        <span className=' mt-4 text-sm md:text-2xl px-10'>Jointly organised by </span>
+         <span className=' text-md font-bold lg:text-2xl px-1'>Maharaja Surajmal Institute of Technology, India </span>
+         <span className=' text-sm lg:text-2xl px-10'>and </span>
+         <span className=' text-md font-bold lg:text-2xl'>Wentworth Institute of Technology, USA</span>
+  
+      <span className=' text-sm  lg:text-2xl px-3 text-center '>Sponsored by <strong>Springer Nature, Book Series-"Algorithms for Intelligent Systems"</strong></span>
+ 
+       
+      <div className='px-2 py-5 lg:py-16 md:px-14 lg:px-28'><img src={pic2} alt="" /></div>
+         
+         <span className='text-xl mt-4 md:mt-5 lg:text-3xl'>CALL FOR PAPERS:</span>
+<span className='mt-3 text-lg text-center lg:text-3xl px-1'>2ND INTERNATIONAL CONFERENCE ON ARTIFICIAL </span>
+<span className='mt-3 text-lg text-center lg:text-3xl px-1'>INTELLIGENCE AND APPLICATIONS (ICAIA 2024)</span>
+<div>
+  <div className=' mt-2 md:mt-8'>
+  <a className=' text-sm text-blue-600 lg:text-2xl px-1' href="https://cmt3.research.microsoft.com/ICAIAconf2024">https://cmt3.research.microsoft.com/ICAIAconf2024</a>
+
+  </div>
+
+
+</div>
+<div className=' mt-2 lg:mt-8 text-sm lg:text-2xl px-10 text-center' >
+We are delighted to invite you to participate in the "International Conference on Artificial Intelligence and Applications (ICAIA 2024)", scheduled for March 19-20, 2024. This premier event will bring together leading researchers, practitioners, and innovators from around the globe to explore the latest advancements and applications in Artificial Intelligence. Please visit <a href=" https://icaia-msit.in/"> <span className='text-blue-600'>https://icaia-msit.in/</span></a> for more information.
+</div>
+<span  className=' mt-10 text-md font-bold lg:text-2xl px-1'>Conference Tracks:</span>
+<div className='mt-8 flex flex-col text-sm  lg:text-2xl'>
+<span> <strong>Track#1: </strong>Evolutionary Algorithms</span>
+<span><strong>Track#2: </strong>Deep AI & Neural Networks</span>
+<span><strong>Track#3: </strong>AI for IOT Applications</span>
+<span><strong>Track#4: </strong>Robotics & AI</span>
+<span><strong>Track#5: </strong>Knowledge Representation</span>
+
+</div>
+
+<span className=' mt-3 md:mt-12 text-blue-600 text-xl lg:text-4xl'>PAPER SUBMISSION</span>
+<span className='mt-3 md:mt-12  text-sm lg:text-2xl px-3 md:px-16 text-center' >Authors are invited to submit full-length (10 pages minimum), original, unpublished papers via the <span className='text-blue-600'>https://cmt3.research.microsoft.com/ICAIAconf2024</span>  and an abstract of at most 200 words. Previously published papers or papers currently under review for other conferences/journals should NOT be submitted and will not be considered. Electronic submission in PDF format to the https://icaia-msit.in/ website is required. Author and contact information (name, affiliation, mailing address, telephone, fax, e-mail) must be entered during submission. All accepted papers will be published in Springer's AIS Book Series.</span>
+
+<span className='mt-3 md:mt-12 text-xl lg:text-4xl text-bold' >IMPORTANT DAYS</span>
+<div className=" border-solid border-b border-black my-2"></div>
+<div className='flex flex-col space-y-2 lg:space-y-3'>
+<blockquote class="mt-3 md:mt-10 text-sm lg:text-xl font-semibold italic text-center text-slate-900">
+<span class="before:block before:absolute before:-inset-0.5 before:bg-cyan-300 relative inline-block">
+    <span class="relative ">Paper Submission Deadline: </span>
+  </span>
+   January 10, 2024
+</blockquote>
+<blockquote class=" text-sm lg:text-xl  font-semibold italic text-center text-slate-900">
+<span class="before:block before:absolute before:-inset-0.5 before:bg-cyan-300 relative inline-block">
+    <span class="relative">Notification of Acceptance:</span>
+  </span>
+  February 10, 2024
+</blockquote>
+<blockquote class=" text-sm lg:text-xl  font-semibold italic text-center text-slate-900">
+<span class="before:block before:absolute before:-inset-0.5 before:bg-cyan-300 relative inline-block">
+    <span class="relative">Camera-Ready Paper Submission: </span>
+  </span>
+  February 20, 2024
+</blockquote>
+<blockquote class=" text-sm lg:text-xl  font-semibold italic text-center text-slate-900">
+<span class="before:block before:absolute before:-inset-0.5 before:bg-cyan-300 relative inline-block">
+    <span class="relative">Conference Dates: </span>
+  </span>
+  March 19-20, 2024
+</blockquote>
+
+</div>
+
+
+<p className=' mt-5 lg:mt-10 text-sm lg:text-2xl md:px-20 text-center' >Join us at ICAIA 2024 to engage in thought-provoking discussions, network with experts, and contribute to advancing Artificial Intelligence. We look forward to your active participation.</p>
+
+    
+  
+
+        </div>
+        
+
+
+     
+         <div className="mt-8 px-4 bg-slate-100 pt-10 pb-20 ">
+      <div className="grid grid-cols-6">
+        <div className=" col-start-1 col-span-3">
+ 
+          <div className="flex flex-col justify-center mt-4 px-8 ">
+            <p className="mt-4 text-md lg:text-2xl text-gray-800">
+            ICAIA 2024 Team,
+            </p>
+            <p className="mt-4 text-md lg:text-2xl text-gray-800">
+            Maharaja Surajmal Institute of Technology,
+            </p>
+            <p className="mt-4 text-md lg:text-2xl text-gray-800">
+            C-4, Janakpuri New Delhi
+            </p>
+          </div>
+        </div>
+      
+          <div className='col-start-5 col-span-4 justify-center items-center order-last'>
+     
+            <ul className="mt-2 space-y-2 ">
+              <li>
+                <a
+                  href="info@icaia-msit.in"
+                  className="text-blue-600 text-md lg:text-2xl transition-colors duration-300 hover:text-deep-purple-accent-400"
+                >
+                info@icaia-msit.in
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/"
+                  className="text-gray-600 text-sm lg:text-xl transition-colors duration-300 hover:text-deep-purple-accent-400"
+                >
+                011-45037193
+                </a>
+              </li>
+              
+            </ul>
+          </div>
+          
+           
+        </div>
+      </div>
+    </div> 
+
+
+    ;
+}
+
+export default Home;
